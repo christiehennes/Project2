dca4c7f3ef93e93253df35450009b5d94bf5475e
Christie Hennes <christiehennes@gmail.com>
2018-08-23 18:54:03 -0700

    Added owner model

---
+++ models/owner.js
@@ -0,0 +1,40 @@
+module.exports = function(sequelize, DataTypes) {
+    let Owner = sequelize.define("Owner", {
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              len: [1]
+            }
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              len: [1]
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              len: [1]
+            }
+        },
+        phone: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              len: [1]
+            }
+        }
+    });
+
+    Owner.associate = function(models) {
+        Owner.hasMany(models.Product, {
+          onDelete: "cascade"
+        });
+    };
+
+    return Owner;
+};
