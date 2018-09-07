9b7a10ad725b7ea8534c3a8c13dfee4fe1942461
Christie Hennes <christiehennes@gmail.com>
2018-09-06 19:32:06 -0700

    Updated express config

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(customAuthMiddleware);
-app.use(express.static(`${clientDir}/public`));
+app.use(express.static(clientDir + '/public'));
 
 
 app.use(express.static("public"));
